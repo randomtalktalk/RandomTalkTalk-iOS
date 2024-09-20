db11a12ac8ecdf583c03de8af6207cbd439f7554
cchanmi <113524@naver.com>
2024-09-20 17:23:01 +0900

    WIP

---
+++ RandomTalkTalk/Project.swift
@@ -0,0 +1,38 @@
+import ProjectDescription
+
+let project = Project(
+    name: "RandomTalkTalk",
+    targets: [
+        .target(
+            name: "RandomTalkTalk",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.RandomTalkTalk",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["RandomTalkTalk/Sources/**"],
+            resources: ["RandomTalkTalk/Resources/**"],
+            dependencies: [
+                .external(name: "Firebase"),
+                .external(name: "ComposableArchitecture"),
+                .external(name: "Moya")
+            ]
+        ),
+        .target(
+            name: "RandomTalkTalkTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.RandomTalkTalkTests",
+            infoPlist: .default,
+            sources: ["RandomTalkTalk/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "RandomTalkTalk")]
+        ),
+    ]
+)
---
+++ RandomTalkTalk/Tuist/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+#if TUIST
+    import ProjectDescription
+
+    let packageSettings = PackageSettings(
+        // Customize the product types for specific package product
+        // Default is .staticFramework
+        // productTypes: ["Alamofire": .framework,] 
+        productTypes: [
+            "Firebase": .framework,
+            "ComposableArchitecture": .framework,
+            "Moya": .framework
+        ]
+    )
+#endif
+
+let package = Package(
+    name: "RandomTalkTalk",
+    dependencies: [
+        // Add your own dependencies here:
+//         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
+        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.2.0"),
+         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.15.0"),
+         .package(url: "https://github.com/Moya/Moya.git", exact: "15.0.0"),
+    ]
+)
