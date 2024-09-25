c098adf2c78446441e3b166be5c4433be0197277
joho2022 <josama2022.dev@gmail.com>
2024-09-25 23:42:53 +0900

    build

--- RandomTalkTalk/RandomTalkTalk/Sources/ContentView.swift
+++ RandomTalkTalk/RandomTalkTalk/Sources/ContentView.swift
@@ -1,11 +1,15 @@
 import SwiftUI
+import FirebaseFirestore
 
 public struct ContentView: View {
     public init() {}
-
+    
     public var body: some View {
         Text("Hello, World!")
             .padding()
+            
+        
+        
     }
 }
 
